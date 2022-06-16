be28e15eb7329c6d6404a7963c34b3a4369617e1
hp <xikmatabdulloh1@gmail.com>
2022-06-16 17:22:50 +0500

    service CRUD from Abul_dev

--- src/main/java/uz/jl/jira/services/auth/MemberService.java
+++ src/main/java/uz/jl/jira/services/auth/MemberService.java
@@ -1,9 +1,69 @@
 package uz.jl.jira.services.auth;
 
+import lombok.NonNull;
+import uz.jl.jira.configs.ApplicationContextHolder;
+import uz.jl.jira.criteria.MemberCriteria;
+import uz.jl.jira.mappers.BaseMapper;
+import uz.jl.jira.repository.AbstractRepository;
+import uz.jl.jira.repository.auth.MemberRepository;
+import uz.jl.jira.repository.auth.ProjectRepository;
+import uz.jl.jira.services.GenericCRUDService;
+import uz.jl.jira.vo.auth.memberVO.MemberCreateVO;
+import uz.jl.jira.vo.auth.memberVO.MemberUpdateVO;
+import uz.jl.jira.vo.auth.memberVO.MemberVO;
+import uz.jl.jira.vo.response.Data;
+import uz.jl.jira.vo.response.ResponseEntity;
+
+import java.util.List;
+
 /**
  * @Author :  Asliddin Ziyodullaev
  * @Date :  16:25   16/06/22
  * @Project :  trelloAlfa
  */
-public class MemberService {
+public class MemberService extends AbstractRepository<MemberRepository, BaseMapper>
+        implements GenericCRUDService<MemberVO, MemberCreateVO, MemberUpdateVO, MemberCriteria, Long> {
+
+    private static MemberService instance;
+
+    protected MemberService(MemberRepository repository, BaseMapper mapper) {
+        super(repository, mapper);
+    }
+
+
+    @Override
+    public ResponseEntity<Data<Long>> create(@NonNull MemberCreateVO dto) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Data<Void>> delete(@NonNull Long aLong) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Data<Void>> update(@NonNull MemberUpdateVO dto) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Data<MemberVO>> findById(@NonNull Long aLong) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Data<List<MemberVO>>> findAll(@NonNull MemberCriteria criteria) {
+        return null;
+    }
+
+    public static MemberService getInstance() {
+        if (instance == null){
+            instance = new MemberService(
+                    ApplicationContextHolder.getBean(MemberRepository.class),
+                    ApplicationContextHolder.getBean(BaseMapper.class)
+            );
+        }
+        return instance;
+    }
+
 }
--- src/main/java/uz/jl/jira/services/auth/ProjectColumnService.java
+++ src/main/java/uz/jl/jira/services/auth/ProjectColumnService.java
@@ -1,10 +1,70 @@
 package uz.jl.jira.services.auth;
 
+import lombok.NonNull;
+import uz.jl.jira.configs.ApplicationContextHolder;
+import uz.jl.jira.criteria.ProjectColumnCriteria;
+import uz.jl.jira.criteria.ProjectCriteria;
+import uz.jl.jira.mappers.BaseMapper;
+import uz.jl.jira.repository.AbstractRepository;
+import uz.jl.jira.repository.auth.ProjectRepository;
+import uz.jl.jira.services.GenericCRUDService;
+import uz.jl.jira.vo.auth.projectColumnVO.ProjectColumnCreateVO;
+import uz.jl.jira.vo.auth.projectColumnVO.ProjectColumnUpdateVO;
+import uz.jl.jira.vo.auth.projectColumnVO.ProjectColumnVO;
+import uz.jl.jira.vo.auth.projectVO.ProjectCreateVO;
+import uz.jl.jira.vo.response.Data;
+import uz.jl.jira.vo.response.ResponseEntity;
+
+import java.util.List;
+
 /**
  * @Author :  Asliddin Ziyodullaev
  * @Date :  16:24   16/06/22
  * @Project :  trelloAlfa
  */
-public class ProjectColumnService {
+public class ProjectColumnService extends AbstractRepository<ProjectRepository, BaseMapper>
+        implements GenericCRUDService<ProjectColumnVO, ProjectColumnCreateVO, ProjectColumnUpdateVO, ProjectColumnCriteria,Long> {
+
+    private static ProjectColumnService instance;
+
+    protected ProjectColumnService(ProjectRepository repository, BaseMapper mapper) {
+        super(repository, mapper);
+    }
+
+
+    @Override
+    public ResponseEntity<Data<Long>> create(@NonNull ProjectColumnCreateVO dto) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Data<Void>> delete(@NonNull Long aLong) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Data<Void>> update(@NonNull ProjectColumnUpdateVO dto) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Data<ProjectColumnVO>> findById(@NonNull Long aLong) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Data<List<ProjectColumnVO>>> findAll(@NonNull ProjectColumnCriteria criteria) {
+        return null;
+    }
+
+    public static ProjectColumnService getInstance() {
+        if (instance == null){
+            instance = new ProjectColumnService(
+                    ApplicationContextHolder.getBean(ProjectRepository.class),
+                    ApplicationContextHolder.getBean(BaseMapper.class)
+            );
+        }
+        return instance;
+    }
 
 }
