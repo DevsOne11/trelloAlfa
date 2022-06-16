12111a1249c0539a6a3f4e20dbb645385c0e40c7
Habibullo <erjigitov92@mail.ru>
2022-06-16 17:16:59 +0500

    Habibullo Service Commit

--- src/main/java/uz/jl/jira/services/auth/ProjectService.java
+++ src/main/java/uz/jl/jira/services/auth/ProjectService.java
@@ -1,9 +1,67 @@
 package uz.jl.jira.services.auth;
 
+import lombok.NonNull;
+import uz.jl.jira.configs.ApplicationContextHolder;
+import uz.jl.jira.criteria.UserCriteria;
+import uz.jl.jira.mappers.BaseMapper;
+import uz.jl.jira.repository.AbstractRepository;
+import uz.jl.jira.repository.auth.ProjectRepository;
+import uz.jl.jira.repository.auth.UserRepository;
+import uz.jl.jira.services.GenericCRUDService;
+import uz.jl.jira.vo.auth.projectVO.ProjectCreateVO;
+import uz.jl.jira.vo.auth.projectVO.ProjectUpdateVO;
+import uz.jl.jira.vo.auth.projectVO.ProjectVO;
+import uz.jl.jira.vo.response.Data;
+import uz.jl.jira.vo.response.ResponseEntity;
+
+import java.util.List;
+
 /**
  * @Author :  Asliddin Ziyodullaev
  * @Date :  16:24   16/06/22
  * @Project :  trelloAlfa
  */
-public class ProjectService {
+public class ProjectService extends AbstractRepository<ProjectRepository, BaseMapper> implements GenericCRUDService<ProjectVO, ProjectCreateVO, ProjectUpdateVO, UserCriteria, Long> {
+
+    private static ProjectService instance;
+
+
+    private ProjectService(ProjectRepository repository, BaseMapper mapper) {
+        super(repository, mapper);
+    }
+
+    @Override
+    public ResponseEntity<Data<Long>> create(@NonNull ProjectCreateVO dto) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Data<Void>> delete(@NonNull Long aLong) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Data<Void>> update(@NonNull ProjectUpdateVO dto) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Data<ProjectVO>> findById(@NonNull Long aLong) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Data<List<ProjectVO>>> findAll(@NonNull UserCriteria criteria) {
+        return null;
+    }
+
+    public static ProjectService getInstance() {
+        if (instance == null) {
+            instance = new ProjectService(
+                    ApplicationContextHolder.getBean(ProjectRepository.class),
+                    ApplicationContextHolder.getBean(BaseMapper.class)
+            );
+        }
+        return instance;
+    }
 }
