4ea75ce81ab94de7d6a18a20ef9493db49223b34
hp <xikmatabdulloh1@gmail.com>
2022-06-16 15:02:52 +0500

    main-controller

---
+++ src/main/java/uz/jl/jira/ui/MainController.java
@@ -0,0 +1,12 @@
+package uz.jl.jira.ui;
+
+/**
+ * name : Abul_dev
+ * data:   Thursday
+ * project name : trelloAlfa
+ */
+public class MainController {
+    public static void main(String[] args) {
+
+    }
+}
